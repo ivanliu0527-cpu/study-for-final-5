c7eeb41f89f4efcc81aac5f239f9ed1d4f3a81c0
ivanliu0527-cpu <ivanliu0527@gmail.com>
2025-12-17 12:33:09 +0800

    Create Meun.py

---
+++ Meun.py
@@ -0,0 +1,22 @@
+import streamlit as st 
+from streamlit_option_meun import option_meun
+
+st.title("Hello student")
+st.write("This is your Python Programming course")
+
+with st.sidebar:
+  selected = option_meun(
+    options = ["Home", "About", " Contact"],
+    icon = ["1-circle-fill",
+            "2-circle-fill",
+            "3-circle-fill"],
+    meun_icon = "emoji-smle-fill",
+    default_index=0,
+  )
+
+if selected == "Home":
+  st.title(f"Welcome to the {selected} page.")
+if selected == "About"
+  st.title(f"Welcome to the {selected} page.")
+if selected == "Contact"
+  st.title(f"Welcome to the {selected} page.")
